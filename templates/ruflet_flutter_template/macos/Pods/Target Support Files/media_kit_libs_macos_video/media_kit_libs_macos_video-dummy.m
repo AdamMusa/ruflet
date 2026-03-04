602a96f54b97486bf5768158ef22d9fdf9831d42
AdamMusa <adammusa2222@gmail.com>
2026-03-04 04:58:24 -0500

    Remove vendored CocoaPods from flutter template

--- templates/ruflet_flutter_template/macos/Pods/Target Support Files/media_kit_libs_macos_video/media_kit_libs_macos_video-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_media_kit_libs_macos_video : NSObject
-@end
-@implementation PodsDummy_media_kit_libs_macos_video
-@end
--- templates/ruflet_flutter_template/macos/Pods/Target Support Files/ruby_runtime/ruby_runtime-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ruby_runtime : NSObject
-@end
-@implementation PodsDummy_ruby_runtime
-@end
