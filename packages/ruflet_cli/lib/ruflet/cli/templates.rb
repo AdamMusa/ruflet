a4edd80204e8acf7520de1c4368a981fa26b2364
AdamMusa <adammusa2222@gmail.com>
2026-03-07 15:51:19 -0500

    update the gemfile for new project to get the lastest stable version of ruflet

--- packages/ruflet_cli/lib/ruflet/cli/templates.rb
+++ packages/ruflet_cli/lib/ruflet/cli/templates.rb
@@ -3,58 +3,42 @@
 module Ruflet
   module CLI
     MAIN_TEMPLATE = <<~RUBY
-      require "ruflet"
-
-      class MainApp < Ruflet::App
-        def initialize
-          super
-          @count = 0
-        end
-
-        def view(page)
-          page.title = "Counter Demo"
-          page.vertical_alignment = Ruflet::MainAxisAlignment::CENTER
-          page.horizontal_alignment = Ruflet::CrossAxisAlignment::CENTER
-          count_text = text(value: @count.to_s, size: 40)
-
-          page.add(
-            container(
-              expand: true,
-              padding: 24,
-              content: column(
-                expand: true,
-                alignment: Ruflet::MainAxisAlignment::CENTER,
-                horizontal_alignment: Ruflet::CrossAxisAlignment::CENTER,
-                spacing: 12,
-                controls: [
-                  text(value: "You have pushed the button this many times:"),
-                  count_text
-                ]
-              )
-            ),
-            appbar: app_bar(
-              title: text(value: "Counter Demo")
-            ),
-            floating_action_button: fab(
-              icon(icon: Ruflet::MaterialIcons::ADD),
-              on_click: ->(_e) {
-                @count += 1
-                page.update(count_text, value: @count.to_s)
-              }
-            )
+    require "ruflet"
+    Ruflet.run do |page|
+      page.title = "Counter Demo"
+      count = 0
+      count_text = nil
+      count_text ||= text(value: count.to_s, size: 40)
+      page.add(
+        container(
+          expand: true,
+          alignment: Ruflet::MainAxisAlignment::CENTER,
+          content: column(
+            alignment: Ruflet::MainAxisAlignment::CENTER,
+            horizontal_alignment: Ruflet::CrossAxisAlignment::CENTER,
+            children: [
+              text(value: "You have pushed the button this many times:"),
+              count_text
+            ]
           )
-        end
-      end
-
-      MainApp.new.run
+        ),
+        floating_action_button: fab(
+          icon(icon: Ruflet::MaterialIcons::ADD),
+          on_click: ->(_e) do
+            count += 1
+            page.update(count_text, value: count.to_s)
+          end
+        )
+      )
+    end
 
     RUBY
 
     GEMFILE_TEMPLATE = <<~GEMFILE
       source "https://rubygems.org"
 
-      gem "ruflet", ">= 0.0.3"
-      gem "ruflet_server", ">= 0.0.3"
+      gem "ruflet", ">= 0.0.6"
+      gem "ruflet_server", ">= 0.0.6"
     GEMFILE
 
     README_TEMPLATE = <<~MD
