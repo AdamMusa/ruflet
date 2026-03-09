7397f607c9e280727eec434a1b773491d6951aa8
AdamMusa <adammusa2222@gmail.com>
2026-03-09 14:51:13 -0400

    added webview in ruflet studio

--- examples/ruflet_studio/sections_media/webview.rb
+++ examples/ruflet_studio/sections_media/webview.rb
@@ -4,11 +4,10 @@ module RufletStudio
   module SectionsMedia
     def build_webview(_page, _status)
       webview_control = web_view(
-        url: "https://flet.dev",
+        url: "https://rubyonrails.org/",
         method: "get",
         expand: true
       )
-
       container(
         expand: true,
         content: webview_control
