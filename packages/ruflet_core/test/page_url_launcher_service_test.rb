ca618fe4c40b0179d3568c18916ed08b6b5b6c3c
AdamMusa <adammusa2222@gmail.com>
2026-05-15 12:20:49 -0400

    Add UrlLauncher service method compatibility

---
+++ packages/ruflet_core/test/page_url_launcher_service_test.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require_relative "test_helper"
+
+class PageUrlLauncherServiceTest < Minitest::Test
+  def test_close_in_app_web_view_uses_flet_url_launcher_signature
+    sent = []
+    page = build_page(sent)
+
+    call_id = page.close_in_app_web_view
+    refute_nil call_id
+
+    invoke_payload = sent.reverse.map(&:last).find { |payload| payload["name"] == "close_in_app_web_view" }
+    refute_nil invoke_payload
+    assert_nil invoke_payload["args"]
+  end
+
+  def test_open_window_uses_flet_url_launcher_signature
+    sent = []
+    page = build_page(sent)
+
+    call_id = page.open_window("https://flet.dev", title: "Flet popup", width: 480, height: 640)
+    refute_nil call_id
+
+    invoke_payload = sent.reverse.map(&:last).find { |payload| payload["name"] == "open_window" }
+    refute_nil invoke_payload
+    assert_equal(
+      {
+        "url" => "https://flet.dev",
+        "title" => "Flet popup",
+        "width" => 480,
+        "height" => 640
+      },
+      invoke_payload["args"]
+    )
+  end
+
+  def test_supports_launch_mode_uses_flet_url_launcher_signature
+    sent = []
+    page = build_page(sent)
+
+    call_id = page.supports_launch_mode("in_app_web_view")
+    refute_nil call_id
+
+    invoke_payload = sent.reverse.map(&:last).find { |payload| payload["name"] == "supports_launch_mode" }
+    refute_nil invoke_payload
+    assert_equal({ "mode" => "in_app_web_view" }, invoke_payload["args"])
+  end
+
+  def test_supports_close_for_launch_mode_uses_flet_url_launcher_signature
+    sent = []
+    page = build_page(sent)
+
+    call_id = page.supports_close_for_launch_mode("in_app_web_view")
+    refute_nil call_id
+
+    invoke_payload = sent.reverse.map(&:last).find { |payload| payload["name"] == "supports_close_for_launch_mode" }
+    refute_nil invoke_payload
+    assert_equal({ "mode" => "in_app_web_view" }, invoke_payload["args"])
+  end
+
+  private
+
+  def build_page(sent)
+    Ruflet::Page.new(
+      session_id: "s1",
+      client_details: { "route" => "/" },
+      sender: ->(action, payload) { sent << [action, payload] }
+    )
+  end
+end
