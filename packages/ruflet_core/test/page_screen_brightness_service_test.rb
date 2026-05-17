b9c298b8c82d3c551e34601f7f59a2adfaf06363
AdamMusa <adammusa2222@gmail.com>
2026-05-17 11:54:16 -0400

    Fix screen brightness service payload

--- packages/ruflet_core/test/page_screen_brightness_service_test.rb
+++ packages/ruflet_core/test/page_screen_brightness_service_test.rb
@@ -28,10 +28,10 @@ class PageScreenBrightnessServiceTest < Minitest::Test
 
   def test_screen_brightness_setters_use_flet_payload_shape
     cases = {
-      set_animate: [false, { "animate" => false }],
-      set_auto_reset: [true, { "auto_reset" => true }],
-      set_application_screen_brightness: [0.75, { "brightness" => 0.75 }],
-      set_system_screen_brightness: [0.25, { "brightness" => 0.25 }]
+      set_animate: [false, { "value" => false }],
+      set_auto_reset: [true, { "value" => true }],
+      set_application_screen_brightness: [0.75, { "value" => 0.75 }],
+      set_system_screen_brightness: [0.25, { "value" => 0.25 }]
     }
 
     cases.each do |method_name, (value, expected_args)|
