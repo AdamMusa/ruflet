a761494c03e1602e61ac26b40e5336b41f25785a
AdamMusa <adammusa2222@gmail.com>
2026-05-18 09:57:54 -0400

    Add magnetometer page service helper

--- packages/ruflet_core/lib/ruflet_ui/ruflet/page.rb
+++ packages/ruflet_core/lib/ruflet_ui/ruflet/page.rb
@@ -700,6 +700,10 @@ module Ruflet
       service(:user_accelerometer, **props)
     end
 
+    def magnetometer(**props)
+      service(:magnetometer, **props)
+    end
+
     def get_application_cache_directory(timeout: nil, on_result: nil)
       invoke_storage_paths("get_application_cache_directory", timeout: timeout, on_result: on_result)
     end
---
+++ packages/ruflet_core/test/page_magnetometer_service_test.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require_relative "test_helper"
+
+class PageMagnetometerServiceTest < Minitest::Test
+  def test_magnetometer_returns_page_service_with_flet_wire_name
+    sent = []
+    page = build_page(sent)
+
+    service = page.magnetometer(enabled: false, cancel_on_error: false, interval: 100)
+
+    assert_equal "magnetometer", service.type
+    assert_equal "Magnetometer", service.to_patch["_c"]
+    assert_equal false, service.props["enabled"]
+    assert_equal false, service.props["cancel_on_error"]
+    assert_equal 100, service.props["interval"]
+    assert_same service, page.service(:magnetometer)
+  end
+
+  def test_magnetometer_keeps_event_handlers
+    sent = []
+    page = build_page(sent)
+
+    service = page.magnetometer(on_reading: ->(_event) {}, on_error: ->(_event) {})
+
+    assert service.has_handler?(:reading)
+    assert service.has_handler?(:error)
+    assert_equal true, service.props["on_reading"]
+    assert_equal true, service.props["on_error"]
+  end
+
+  private
+
+  def build_page(sent)
+    Ruflet::Page.new(
+      session_id: "s1",
+      client_details: { "route" => "/" },
+      sender: ->(action, payload) { sent << [action, payload] }
+    )
+  end
+end
