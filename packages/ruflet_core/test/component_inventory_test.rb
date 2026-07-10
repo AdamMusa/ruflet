711e8c487702d478fcc891a2ae6003018c594e4e
AdamMusa <adammusa2222@gmail.com>
2026-07-10 16:23:59 -0400

    Guard Python Flet control test coverage

--- packages/ruflet_core/test/component_inventory_test.rb
+++ packages/ruflet_core/test/component_inventory_test.rb
@@ -2,6 +2,7 @@
 
 require_relative "test_helper"
 require_relative "conformance/component_inventory"
+require "json"
 
 class RufletComponentInventoryTest < Minitest::Test
   def test_all_python_flet_controls_are_implemented_or_classified
@@ -14,5 +15,20 @@ class RufletComponentInventoryTest < Minitest::Test
     inventory = RufletFletComponentInventory.inventory
 
     assert_equal [], inventory[:service_deferred]
+  end
+
+  def test_implemented_python_controls_have_obvious_test_or_conformance_case
+    inventory = RufletFletComponentInventory.inventory
+    conformance_classes = JSON.parse(File.read(File.expand_path("conformance/flet_cases.json", __dir__)))
+                              .fetch("cases")
+                              .map { |test_case| test_case.fetch("python_class") }
+                              .to_set
+    test_text = Dir[File.expand_path("*_test.rb", __dir__)].map { |file| File.read(file) }.join("\n")
+
+    untested = inventory[:implemented].reject do |name|
+      conformance_classes.include?(name) || test_text.match?(/#{Regexp.escape(name)}/i)
+    end
+
+    assert_equal [], untested
   end
 end
